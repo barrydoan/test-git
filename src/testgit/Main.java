70c5bc788a447c951bb5f0cc4d4a9df57a2db332
Barry Doan <barry.n.doan@gmail.com>
2021-01-30 18:53:21 -0500

    commit tast 1

--- src/testgit/Main.java
+++ src/testgit/Main.java
@@ -7,5 +7,6 @@ public class Main {
 
     public static void function1() {
         System.out.print("This is the function 1 code");
+        System.out.print("This is the function 1 code");
     }
 }
