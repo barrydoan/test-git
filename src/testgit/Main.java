1e78ddc56879108f25cfadd1b56ae5f1267118ff
cindyhongtran <cindytran311@gmail.com>
2021-01-30 18:59:15 -0500

    Task 2

--- src/testgit/Main.java
+++ src/testgit/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("This is the first command");
     }
+	
+	public static void task2() {
+		System.out.print("This is the first 2");
+	}
 }
