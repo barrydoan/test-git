40626073c05c647b874504dcfc662573f5667350
Barry Doan <barry.n.doan@gmail.com>
2021-01-30 18:31:16 -0500

    Commit to task 1

--- src/testgit/Main.java
+++ src/testgit/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("This is the first command");
     }
+
+    public static void function1() {
+        System.out.print("This is the function 1 code");
+    }
 }
