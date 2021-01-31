3b2c1be497b096ad6efc8bd1420eae4d7a2be758
cindyhongtran <cindytran311@gmail.com>
2021-01-30 19:18:51 -0500

    Push to master

--- src/testgit/Main.java
+++ src/testgit/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("This is the first command");
     }
-
-    public static void function1() {
+	
+	public static void task2() {
+		System.out.print("This is the first 2");
+	}
+	
+	public static void function1() {
         System.out.print("This is the function 1 code");
         System.out.print("This is the function 2 code");
         System.out.print("This is the function 3 code");
